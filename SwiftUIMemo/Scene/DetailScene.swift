28bf0d146f6967144ddd17e54a9e5e49c54903ef
Jeongmin <tdcian71@gmail.com>
2021-05-04 08:20:06 +0900

    메모 보기 화면

---
+++ SwiftUIMemo/Scene/DetailScene.swift
@@ -0,0 +1,42 @@
+//
+//  DetailScene.swift
+//  SwiftUIMemo
+//
+//  Created by APPLE on 2021/05/04.
+//
+
+import SwiftUI
+
+struct DetailScene: View {
+    @ObservedObject var memo: Memo
+    @EnvironmentObject var store: MemoStore
+    @EnvironmentObject var formatter: DateFormatter
+    
+    var body: some View {
+        VStack {
+            ScrollView {
+                VStack {
+                    HStack {
+                        Text(self.memo.content)
+                            .padding()
+                        
+                        Spacer()
+                    }
+                    Text("\(self.memo.insertDate, formatter: formatter)")
+                        .padding()
+                        .font(.footnote)
+                        .foregroundColor(Color(UIColor.secondaryLabel))
+                }
+            }
+        }
+        .navigationBarTitle("메모 보기")
+    }
+}
+
+struct DetailScene_Previews: PreviewProvider {
+    static var previews: some View {
+        DetailScene(memo: Memo(content: "SwiftUI"))
+            .environmentObject(MemoStore())
+            .environmentObject(DateFormatter.memoDateFormatter)
+    }
+}
